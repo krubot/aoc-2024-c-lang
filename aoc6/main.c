763a941af428e517f8a830f604e2d2259baa95b6
Ben Eustace <krubot.ops@gmail.com>
2024-12-25 18:59:21 +0000

    Adding answer for part 2 of aoc 6

--- aoc6/main.c
+++ aoc6/main.c
@@ -1,157 +1,207 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #define MAX_LENGTH 1000
 #define MAX_HEIGHT 1000
+#define FILE_NAME "input.txt"
 
-enum Direction {
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT
+enum Direction { UP, DOWN, LEFT, RIGHT };
+
+struct Field {
+  char **field;
+  int fieldLength;
+  int fieldHeight;
 };
 
 struct Map {
-	char** field;
-	int fieldLength;
-	int fieldHeight;
-	enum Direction direction;
-  bool outOfBounds;	
+  char **field;
+  int fieldLength;
+  int fieldHeight;
+  enum Direction direction;
+  bool outOfBounds;
 };
 
-struct Map next(struct Map map) {
-	int x;
-	int y;
+// Function prototypes
+struct Field fieldInit(void);
+void freeField(char **field, int height);
+struct Map next(struct Map map);
 
-	for (int i = 0; i < map.fieldHeight; i++) {
-		for (int j = 0; j < map.fieldLength; j++) {
-			if (map.field[i][j] == '^') {
-				x = i;
-				y = j;
-			}
+struct Map next(struct Map map) {
+  int x = -1, y = -1;
+
+  // Find the '^' position
+  for (int i = 0; i < map.fieldHeight; i++) {
+    for (int j = 0; j < map.fieldLength; j++) {
+      if (map.field[i][j] == '^') {
+        x = i;
+        y = j;
+        break;
+      }
+    }
+    if (x != -1) break;
+  }
+
+  if (x == -1 || y == -1) { // No '^' found, mark out of bounds
+    map.outOfBounds = true;
+    return map;
+  }
+
+  if (map.direction == UP) {
+    if (x <= 0) {
+      map.outOfBounds = true;
+      map.field[x][y] = 'X';
+    } else if (map.field[x - 1][y] == '#') {
+      map.direction = RIGHT;
+    } else {
+      map.field[x - 1][y] = '^';
+      map.field[x][y] = 'X';
+    }
+  } else if (map.direction == RIGHT) {
+    if (y + 1 >= map.fieldLength) {
+      map.outOfBounds = true;
+      map.field[x][y] = 'X';
+    } else if (map.field[x][y + 1] == '#') {
+      map.direction = DOWN;
+    } else {
+      map.field[x][y + 1] = '^';
+      map.field[x][y] = 'X';
+    }
+  } else if (map.direction == DOWN) {
+    if (x + 1 >= map.fieldHeight) {
+      map.outOfBounds = true;
+      map.field[x][y] = 'X';
+    } else if (map.field[x + 1][y] == '#') {
+      map.direction = LEFT;
+    } else {
+      map.field[x + 1][y] = '^';
+      map.field[x][y] = 'X';
+    }
+  } else { // LEFT
+    if (y <= 0) {
+      map.outOfBounds = true;
+      map.field[x][y] = 'X';
+    } else if (map.field[x][y - 1] == '#') {
+      map.direction = UP;
+    } else {
+      map.field[x][y - 1] = '^';
+      map.field[x][y] = 'X';
     }
   }
 
-	if (map.direction == UP) {
-		if (x <= 0) {
-			map.outOfBounds = true;
-			map.field[x][y] = 'X';
-		} else {	
-			if (map.field[x-1][y] == '#') {
-				map.direction = RIGHT;
-			} else {
-				map.field[x-1][y] = '^';
-				map.field[x][y] = 'X';
-			}
-		}
-	} else if (map.direction == RIGHT) {
-		if (y + 1 >= map.fieldLength) {
-			map.outOfBounds = true;
-			map.field[x][y] = 'X';
-		} else {
-			if (map.field[x][y+1] == '#') {
-				map.direction = DOWN;
-			} else {
-				map.field[x][y+1] = '^';
-				map.field[x][y] = 'X';
-			}
-		}
-	} else if (map.direction == DOWN) {
-		if (x + 1 >= map.fieldHeight) {
-			map.outOfBounds = true;
-			map.field[x][y] = 'X';
-		} else {
-			if (map.field[x+1][y] == '#') {
-				map.direction = LEFT;
-			} else {
-				map.field[x+1][y] = '^';
-				map.field[x][y] = 'X';
-			}
-		}
-	} else {
-		if (y <= 0) {
-			map.outOfBounds = true;
-			map.field[x][y] = 'X';
-		} else {
-			if (map.field[x][y-1] == '#') {
-				map.direction = UP;
-			} else {
-				map.field[x][y-1] = '^';
-				map.field[x][y] = 'X';
-			}
-		}
-	}
+  return map;
+}
 
-	return map;
+struct Field fieldInit(void) {
+  FILE *file = fopen(FILE_NAME, "r");
+  if (!file) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  char ch;
+  int i = 0, j = 0, arrLength = 0, arrHeight = 0;
+
+  char **arr = (char **)malloc(MAX_LENGTH * sizeof(char *));
+  for (int k = 0; k < MAX_LENGTH; k++) {
+    arr[k] = (char *)malloc(MAX_HEIGHT * sizeof(char));
+  }
+
+  while ((ch = fgetc(file)) != EOF) {
+    if (ch != '\n') {
+      arr[i][j] = ch;
+      j++;
+      if (j > arrLength) {
+        arrLength = j;
+      }
+    } else {
+      i++;
+      j = 0;
+      if (i > arrHeight) {
+        arrHeight = i;
+      }
+    }
+  }
+
+  fclose(file);
+
+  struct Field field = {
+      .field = arr,
+      .fieldHeight = arrHeight + 1, // Adjust for 0-indexing
+      .fieldLength = arrLength,
+  };
+
+  return field;
+}
+
+void freeField(char **field, int height) {
+  for (int i = 0; i < height; i++) {
+    free(field[i]);
+  }
+  free(field);
 }
 
 int main(void) {
-	FILE *file = fopen("input.txt","r");
+  struct Field field = fieldInit();
 
-	if (file == NULL) {
-		perror("File open failed");
-		return EXIT_FAILURE;	
-	}
+  struct Map map = {
+      .field = field.field,
+      .fieldLength = field.fieldLength,
+      .fieldHeight = field.fieldHeight,
+      .direction = UP,
+      .outOfBounds = false,
+  };
 
-	char ch;
-	int i = 0;
-	int j = 0;
-	int arrLength = 0;
-	int arrHeight = 0;
-
-	char** arr = (char**)malloc(MAX_LENGTH * sizeof(char*));
-
-  for (int i = 0; i < MAX_LENGTH; i++) {
-		arr[i] = (char*)malloc(MAX_HEIGHT * sizeof(char));
-	}
-
-	while((ch = fgetc(file)) != EOF) {
-		if (ch != '\n') {
-			arr[i][j] = ch;
-			j++;
-			if (j > arrLength) {
-				arrLength = j;
-			}
-		} else {
-			i++;
-			j = 0;
-			if (i > arrHeight) {
-				arrHeight = i;
-			}
-		}
-	}
-
-	fclose(file);
-
-	struct Map map = {
-		.field = arr,
-		.fieldLength = arrLength,
-		.fieldHeight = arrHeight,
-		.direction = UP,
-		.outOfBounds = false,
-	};
-
-	while (!map.outOfBounds) {
-		map = next(map);
-	}
-
-	int count = 0;
-
-	for (int i = 0; i < arrLength; i++) {
-		for (int j = 0; j < arrHeight; j++) {
-			if (map.field[i][j] == 'X') {
-				count++;
-			}
-		}
-	}
-
-	printf("answer1: %d\n",count);
-
-	for (int k = 0; k < MAX_HEIGHT; k++) {
-		free(arr[k]);
+  while (!map.outOfBounds) {
+    map = next(map);
   }
-  free(arr);
 
-	return 0;
+  int count = 0;
+  for (int i = 0; i < field.fieldHeight; i++) {
+    for (int j = 0; j < field.fieldLength; j++) {
+      if (map.field[i][j] == 'X') {
+        count++;
+      }
+    }
+  }
+  printf("answer1: %d\n", count);
+
+  // TODO: improve speed and memory profile 
+  int count2 = 0;
+
+  for (int i = 0; i < field.fieldHeight; i++) {
+    for (int j = 0; j < field.fieldLength; j++) {
+      if (field.field[i][j] != '^') {
+        struct Field tempField = fieldInit();
+        tempField.field[i][j] = '#';
+
+        struct Map tempMap = {
+            .field = tempField.field,
+            .fieldLength = tempField.fieldLength,
+            .fieldHeight = tempField.fieldHeight,
+            .direction = UP,
+            .outOfBounds = false,
+        };
+
+        for (int k = 0; k < 10000; k++) {
+          tempMap = next(tempMap);
+          if (tempMap.outOfBounds) {
+            break;
+          }
+        }
+
+        if (!tempMap.outOfBounds) {
+          count2++;
+        }
+
+        freeField(tempField.field, tempField.fieldHeight);
+      }
+    }
+  }
+
+  printf("answer2: %d\n", count2);
+
+  freeField(field.field, field.fieldHeight);
+  return 0;
 }
+
